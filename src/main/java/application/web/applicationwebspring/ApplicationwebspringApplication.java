309c7acd6a945e471a51f5390d2dd0f077105354
thaseda saidani <thasedasaidani2000@gmail.com>
2025-02-26 23:53:30 +0100

    Initial commit

---
+++ src/main/java/application/web/applicationwebspring/ApplicationwebspringApplication.java
@@ -0,0 +1,13 @@
+package application.web.applicationwebspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationwebspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationwebspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/application/web/applicationwebspring/ApplicationwebspringApplicationTests.java
@@ -0,0 +1,13 @@
+package application.web.applicationwebspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationwebspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
